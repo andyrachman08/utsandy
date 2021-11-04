97dd5285b688d75c0d7f8ad71c1b4808d95e24fe
andyrachman08 <mandyrchmn@gmail.com>
2021-11-05 04:35:13 +0700

    first commit

---
+++ page/tentang.php
@@ -0,0 +1,38 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <img src="img/pace.png" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: Moch Andy Rachman</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>NIM</b></li>
+        </td>
+        <td>: 1931710109</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: D3 Manajemen Informaika</td>
+      </ul>
+    </tr>
+
+  </table>
+
+</div>
